9b889f4b8bb5dc134265f1b3e300caf3c6689ed2
iamqq <sergejr@gmail.com>
2014-10-03 21:46:13 +0600

    Form1

--- WebKiosk/Form1.Designer.cs
+++ WebKiosk/Form1.Designer.cs
@@ -40,7 +40,6 @@
             this.webC.Size = new System.Drawing.Size(234, 207);
             this.webC.Source = new System.Uri("http://yandex.ru/", System.UriKind.Absolute);
             this.webC.TabIndex = 0;
-            this.webC.ShowCreatedWebView += new Awesomium.Core.ShowCreatedWebViewEventHandler(this.Awesomium_Windows_Forms_WebControl_ShowCreatedWebView);
             // 
             // button1
             // 
@@ -61,7 +60,6 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
